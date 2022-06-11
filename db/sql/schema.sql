45e69f65050ded5257bb2e0eb7f10cadccebc029
Head Hat <meads@users.noreply.github.com>
2022-06-10 19:46:57 -0500

    migrations

--- db/sql/schema.sql
+++ db/sql/schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE images (
   "id"   BIGSERIAL PRIMARY KEY,
   "name" TEXT NULL,
   "data" TEXT      NOT NULL,
-  "created" NVARCHAR(8) NOT NULL,
+  "created" VARCHAR(8) NOT NULL,
   "deleted" BOOLEAN DEFAULT FALSE
 );
