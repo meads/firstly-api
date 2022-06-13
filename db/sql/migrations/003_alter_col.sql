a40bef26d60a341e5c729929af6536d99bff9f2f
Head Hat <meads@users.noreply.github.com>
2022-06-12 21:20:51 -0500

    sql migrations

--- db/sql/migrations/003_alter_col.sql
+++ db/sql/migrations/003_alter_col.sql
@@ -1,4 +1,4 @@
 -- +migrate Up
 ALTER TABLE "images" DROP COLUMN "name";
 ALTER TABLE "images" DROP COLUMN deleted;
-ALTER TABLE "images" ADD  COLUMN deleted INT;
+ALTER TABLE "images" ADD  COLUMN deleted INT NOT NULL;
