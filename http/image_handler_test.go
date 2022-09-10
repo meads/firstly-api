50a1267977ece9b3c3aa6f26ddc3b6d16f59bb2f
Head Hat <meads@users.noreply.github.com>
2022-09-10 11:09:08 -0500

    adds remaining tests for the image handler

--- http/image_handler_test.go
+++ http/image_handler_test.go
@@ -3,6 +3,7 @@ package http
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -14,11 +15,10 @@ import (
 	"github.com/meads/firstly-api/db"
 )
 
-func TestImageAPI(t *testing.T) {
+func TestImageHandler(t *testing.T) {
 
 	tests := []struct {
 		body              *bytes.Buffer
-		callback          func(w *httptest.ResponseRecorder) bool
 		method            string
 		name              string
 		responseCode      int
@@ -29,11 +29,8 @@ func TestImageAPI(t *testing.T) {
 			body: func() *bytes.Buffer {
 				return bytes.NewBufferString("{\"data\":\"test\"}")
 			}(),
-			callback: func(w *httptest.ResponseRecorder) bool {
-				return true
-			},
 			method:       http.MethodPost,
-			name:         "should create image record when valid data supplied",
+			name:         "should respond with Status Code 200 when valid data supplied",
 			responseCode: http.StatusOK,
 			route:        "/image/",
 			setupExpectations: func(store *db.MockStore) {
@@ -46,9 +43,33 @@ func TestImageAPI(t *testing.T) {
 					}, nil)
 			},
 		},
+		{
+			name: "should respond with Status Code 400 given no name is supplied",
+			body: func() *bytes.Buffer {
+				return bytes.NewBufferString("{\"data\":\"\"}")
+			}(),
+			method:       http.MethodPost,
+			responseCode: http.StatusBadRequest,
+			route:        "/image/",
+			setupExpectations: func(store *db.MockStore) {
+			},
+		},
+		{
+			name: "should respond with Status Code 500 given there is some server error",
+			body: func() *bytes.Buffer {
+				return bytes.NewBufferString("{\"data\":\"server error\"}")
+			}(),
+			method:       http.MethodPost,
+			responseCode: http.StatusInternalServerError,
+			route:        "/image/",
+			setupExpectations: func(store *db.MockStore) {
+				store.EXPECT().Create(gomock.Any(), "server error").Return(db.Image{}, errors.New("oops"))
+			},
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			// Arrange
 			router := gin.Default()
 			gin.SetMode(gin.TestMode)
 
@@ -63,6 +84,7 @@ func TestImageAPI(t *testing.T) {
 			request := httptest.NewRequest(test.method, test.route, test.body)
 			router.ServeHTTP(responseRecorder, request)
 
+			// Assert
 			assert.Equal(t, test.responseCode, responseRecorder.Code)
 
 			response := db.Image{}
@@ -74,7 +96,6 @@ func TestImageAPI(t *testing.T) {
 			if err := json.NewDecoder(responseRecorder.Body).Decode(&response); err != nil {
 				t.Errorf("Error decoding response body: %v", err)
 			}
-
 		})
 	}
 }
