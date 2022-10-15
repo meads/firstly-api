7f7174e0fc92ce754b14fcb8842b7e4317b56205
Head Hat <meads@users.noreply.github.com>
2022-10-15 08:39:46 -0500

    split out web client into separate app

--- www/assets/notify.js
+++
@@ -1,9 +0,0 @@
-var notify = (function(){
-    function success(message) {
-        alert(message)
-    }
-    function error(message) {
-        alert(message)
-    }
-    return { success, error }
-}())
