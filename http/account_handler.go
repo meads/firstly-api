aefdad86299e1b6c9ae40fa0cfef0c6d0f72f1f6
Head Hat <meads@users.noreply.github.com>
2022-10-19 16:48:29 -0500

    create account calls jwt signin on success

--- http/account_handler.go
+++ http/account_handler.go
@@ -4,7 +4,9 @@ import (
 	"errors"
 	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	db "github.com/meads/firstly-api/db"
 	"github.com/meads/firstly-api/security"
@@ -51,9 +53,37 @@ func (server *FirstlyServer) CreateAccountHandler(store db.Store, hasher securit
 			return
 		}
 
-		ctx.JSON(http.StatusOK, struct {
-			username string
-		}{account.Username})
+		// Declare the expiration time of the token
+		// here, we have kept it as 5 minutes
+		expirationTime := time.Now().Add(5 * time.Minute)
+
+		// Create the JWT claims, which includes the username and expiry time
+		claims := &Claims{
+			Username: account.Username,
+			StandardClaims: jwt.StandardClaims{
+				// In JWT, the expiry time is expressed as unix milliseconds
+				ExpiresAt: expirationTime.Unix(),
+			},
+		}
+
+		// Declare the token with the algorithm used for signing, and the claims
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+		// Create the JWT string
+		tokenString, err := token.SignedString(jwtKey)
+		if err != nil {
+			// If there is an error in creating the JWT return an internal server error
+			ctx.Writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		// Finally, we set the client cookie for "token" as the JWT we just generated
+		// we also set an expiry time which is the same as the token itself
+		http.SetCookie(ctx.Writer, &http.Cookie{
+			Name:    "token",
+			Value:   tokenString,
+			Expires: expirationTime,
+		})
 	}
 }
 
